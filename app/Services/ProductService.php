0f50aee85fbe8ca35933da04241cd2faa8c213ff
MatiasOrcajo <matiasorcajo3@gmail.com>
2026-05-07 20:37:17 -0300

    Refactor product creation logic in ProductService to improve maintainability

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -7,33 +7,30 @@ use App\Models\Product;
 class ProductService
 {
 
+
+    private function create(Product $product, string $name, int $quantity, string $measure)
+    {
+        $product->name = trim($name);
+        $product->quantity = $this->normalizeDecimal($quantity);
+        $product->measure = $measure;
+        $product->save();
+    }
+
     public function normalizeProductsNamesByGrams()
     {
         $products = Product::all();
-//        $products = Product::where('name', 'Protector solar labial humectante NIVEA Sun Protect FPS 50 para todo tipo de piel x 4,8 grs')->get();
         $gramsArray = ['g', 'g.', 'gr.', 'gr', 'grm', 'grm.', 'grs', 'grs.'];
 
-
         foreach ($products as $product) {
-
             $explode = explode(' ', $product->name);
-
             foreach ($explode as $index => $word) {
-
-                $newProductName = "";
-
                 // 1. Intentar capturar número y unidad pegados
                 if (preg_match('/^(\d+(?:,\d+)?)([a-zA-Z.]+)$/', $word, $matches)) {
                     if (in_array(strtolower($matches[2]), $gramsArray)) {
                         dump("Encontrado pegado: " . $matches[1] . " " . $matches[2]);
                         $quantity = $matches[1];
-
                         $newProductName = str_replace($matches[0], "", $product->name);
-
-                        $product->name = trim($newProductName);
-                        $product->quantity = $this->normalizeDecimal($quantity);
-                        $product->measure = 'g';
-                        $product->save();
+                        $this->create($product, $newProductName, $this->normalizeDecimal($quantity), 'g');
                     }
                 } // 2. Si la palabra es SOLO un número, mirar la palabra siguiente
                 elseif (preg_match('/^\d+(?:,\d+)?$/', $word, $numberMatch)) {
@@ -42,12 +39,7 @@ class ProductService
                         dump("Encontrado separado: " . $numberMatch[0] . " " . $nextWord);
                         $quantity = $numberMatch[0];
                         $newProductName = str_replace([$word, $explode[$index+1]], "", $product->name);
-                        $newProductName = trim($newProductName);
-
-                        $product->name = trim($newProductName);
-                        $product->quantity = $this->normalizeDecimal($quantity);
-                        $product->measure = 'g';
-                        $product->save();
+                        $this->create($product, $newProductName, $this->normalizeDecimal($quantity), 'g');
                     }
                 }
             }
